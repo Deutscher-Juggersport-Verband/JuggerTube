1304906345b6d75317d04fc0820d3a6cfdbbd672
nalochen <unger.nalo@gmx.de>
2025-05-12 14:45:15 +0200

    [42-frontend-styling] delete unused code

--- frontend/apps/desktop/src/app/info-bar/info-bar.component.ts
+++ frontend/apps/desktop/src/app/info-bar/info-bar.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'info-bar',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './info-bar.component.html',
   styleUrl: './info-bar.component.less',
